b93a38c8357e8e2175ef2545675707a88fb8dcf2
Noosheen <91496170+N-SHarifi1995@users.noreply.github.com>
2022-04-15 12:49:04 +0430

    Add files via upload

---
+++ animation navbar/script/script.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    let menue = document.querySelector('.fullmenue');
+    let button = document.querySelector('.menuicon');
+
+
+
+    menue.classList.toggle('active');
+    button.classList.toggle('active');
+}
